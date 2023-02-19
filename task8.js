4c5af76c82d6b369c8c6f9f180d93773217c04b9
qwertyuiop098ghdjksjhkdsjn <anastasia.spasova@mail.ru>
2023-02-19 13:59:42 +0300

    second commit

--- task8.js
+++ task8.js
@@ -1,8 +1,9 @@
-let arr = new Map([
-    ["keys1", "values1"],
-    ["keys2", "values2"],
-    ["keys3", "values3"]
-    ]);                                     
-    for (let name of arr) {
-    console.log(name);
-    }
+let arr = new Map();
+arr.set("keys1", "values1");
+arr.set("keys2", "values2");
+arr.set("keys3", "values3");
+
+
+for (let name of arr.keys()){
+    console.log(`Ключ — ${name}, значение — ${arr.get(name)}`);
+}
